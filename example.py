91330e6a1ebbdcb07eeb2c16e4e48df530e44c78
Thies Gehrmann <t.gehrmann@tudelft.nl>
2013-09-12 09:53:35 +0200

    Can export data to circos now

--- example.py
+++ example.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python
 
+import proteny as pp;
+import data;
+
+sc = data.schco2();
+ag = data.agabi();
+
+PR = pp.proteny();
+
+PR.add_org(*sc, isrep=True);
+PR.add_org(*ag, isrep=True);
+
+PR.blast();
+
 
--- proteny.py
+++ proteny.py
@@ -1,9 +1,12 @@
+import sys;
+from ibidas import *;
+
+sys.path.append('./utils');
+import seq as sequtils;
 
 ###############################################################################
 
-class synteny:
-
-  import utils.seq as seq;
+class proteny:
 
   ###############################################################################
 
@@ -26,9 +29,9 @@ class synteny:
 
   ###############################################################################
 
-  def add_org(self, name, genome_filename, genes_filename):
-    genome = self.load_genome(genome_filename);
-    genes  = self.load_gene_degs(genes_filename);
+  def add_org(self, name, genes_filename, genome_filename, isrep=False):
+    genome = self.load_genome(genome_filename, isrep=isrep);
+    genes  = self.load_gene_defs(genes_filename, isrep=isrep);
 
     self.org_names.append(name);
     self.org_genomes.append(genome);
@@ -40,19 +43,20 @@ class synteny:
   ###############################################################################
 
   __gene_slice_names__ = ( 'chrid', 'start', 'end', 'strand', 'geneid', 'transcriptid', 'exonid');
-  def load_gene_defs(self, filename):
-    return (Read(filename) / self.__gene_slice_names__).Detect().Copy();
+  def load_gene_defs(self, data, isrep=False):
+    return ((data if isrep else Read(data)) / self.__gene_slice_names__).Detect().Copy();
   #edef
 
   ###############################################################################
 
   __genome_slice_names__ = ('chrid', 'sequence');
-  def load_genome(self, filename):
-    return (Read(filename) / self.__genome_slice_names__).Detect().Copy();
+  def load_genome(self, data, isrep=False):
+    return ((data if isrep else Read(data)) / self.__genome_slice_names__).Detect().Copy();
   #edef
 
   #############################################################################
 
+  __exon_slice_names__ = __gene_slice_names__ + ('sequence',)
   def translate_exons(self, genome, genes):
 
     EG = genes.GroupBy(_.transcriptid).Get(_.chrid[0], _.start, _.end, _.strand[0], _.geneid[0], _.transcriptid, _.exonid).Copy();
@@ -69,11 +73,11 @@ class synteny:
       j = 0;
       for (s, e, id) in sorted(zip(starts, ends, exonids), key=lambda x: x[2], reverse=False):
         tr_ex       = chrdna[s-1:e];
-        transcript += seq.revcomp(tr_ex) if (strand == '-') else tr_ex;
+        transcript += sequtils.revcomp(tr_ex) if (strand == '-') else tr_ex;
         exonsource += [exonsource[-1] + int((e-(s-1))/3)];
       #efor
       
-      aa_seq = seq.translate(transcript);
+      aa_seq = sequtils.translate(transcript);
       for (s, e, id) in sorted(zip(starts, ends, exonids), key=lambda x: x[2], reverse=False):
         ex_s = exonsource[id-1];
         ex_e = exonsource[id];
@@ -81,7 +85,7 @@ class synteny:
       #efor
     #efor
 
-    return (Rep(exons) / ('chrid', 'start', 'end', 'strand', 'geneid', 'transcriptid', 'exonid', 'sequence')).Detect().Copy();
+    return (Rep(exons) / self.__exon_slice_names__).Detect().Copy();
   #edef
 
   ###############################################################################
@@ -105,7 +109,7 @@ class synteny:
       j = 0;
       for (s, e, id) in sorted(zip(starts, ends, exonids), key=lambda x: x[2], reverse=False):
         tr_ex       = chrdna[s-1:e];
-        transcript += seq.revcomp(tr_ex) if (strand == '-') else tr_ex;
+        transcript += sequtils.revcomp(tr_ex) if (strand == '-') else tr_ex;
         exonsource += [exonsource[-1] + int((e-(s-1))/3)];
       #efor
       for i in xrange(0, len(transcript), 3):
@@ -122,34 +126,35 @@ class synteny:
 
   ###############################################################################
 
+  __blast_slice_names__ = ('a_chrid', 'a_strand', 'a_geneid', 'a_transcriptid', 'a_exonid', 'b_chrid', 'b_strand', 'b_geneid', 'b_transcriptid', 'b_exonid', 'qstart', 'qend', 'sstart', 'send', 'pident', 'mismatch', 'gapopen', 'evalue', 'bitscore');
   def blast(self, id_a = 0, id_b=1):
     if len(self.org_names) < 2:
-      echo "Cannot run BLAST, too few organisms added!";
+      print "Cannot run BLAST, too few organisms added!";
     #fi
 
-    a_exons = self.org_exons[id_a][_.end - _.start > 60] / tuple([ 'a_' + s for s in self.__gene_slice_names__]);
-    b_exons = self.org_exons[id_b][_.end - _.start > 60] / tuple([ 'b_' + s for s in self.__gene_slice_names__]);
+    a_exons = self.org_exons[id_a][_.end - _.start > 60] / tuple([ 'a_' + s for s in self.__exon_slice_names__]);
+    b_exons = self.org_exons[id_b][_.end - _.start > 60] / tuple([ 'b_' + s for s in self.__exon_slice_names__]);
     
     print "Running BLAST for %s v %s" % (self.org_names[id_a], self.org_names[id_b]);
     R = a_exons | Blast(reciprocal = True, normalize=True, folder='./blast_runs/') | b_exons
     R = R.Copy();
-    filename = 'results.%s.%s.blast' % (self.org_names[id_a], self.org_names[id_b]);
-    self.blast_results.append((id_a, id_b, filename));
-    Save(R, filename);
-    R = Load(filename);
+    #filename = 'results.%s.%s.blast' % (self.org_names[id_a], self.org_names[id_b]);
+    #self.blast_results.append((id_a, id_b, filename));
+    #Save(R, filename);
+    #R = Load(filename);
     
-    F = R.Get((_.a_assemblyid, _.a_strand, _.a_proteinid, _.a_exonid), \
-              (_.b_assemblyid, _.b_strand, _.b_proteinid, _.b_exonid), \
-              (_.a_start + (_.qstart * 3 )),                           \
-              (_.a_start + (_.qend * 3 )),                             \
-              (_.b_start + (_.sstart * 3 )),                           \
-              (_.b_start + (_.send * 3 )),                             \
-              _.pident,                                                \
-              _.mismatch,                                              \
-              _.gapopen,                                               \
-              _.evalue,                                                \
+    F = R.Get(_.a_chrid, _.a_strand, _.a_geneid, _.a_transcriptid, _.a_exonid, \
+              _.b_chrid, _.b_strand, _.b_geneid, _.b_transcriptid, _.b_exonid, \
+              (_.a_start + (_.qstart * 3 )),                         \
+              (_.a_start + (_.qend * 3 )),                           \
+              (_.b_start + (_.sstart * 3 )),                         \
+              (_.b_start + (_.send * 3 )),                           \
+              _.pident,                                              \
+              _.mismatch,                                            \
+              _.gapopen,                                             \
+              _.evalue,                                              \
               _.bitscore);
-    F = ( F / ('org_a', 'org_b', 'qstart', 'qend', 'sstart', 'send', 'pident', 'mismatch', 'gapopen', 'evalue', 'bitscore') ).Copy();
+    F = ( F / self.__blast_slice_names__).Copy()
     self.blast_hits[(id_a, id_b)] = F;
   #edef
 
