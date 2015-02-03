2c5b1c57f7f0f4e2f812021669caf9c11d9087b2
Thies Gehrmann <t.gehrmann@tudelft.nl>
2015-02-03 17:15:51 +0100

    Added wrappers

---
+++ run_proteny.py
@@ -0,0 +1,94 @@
+#!/bin/env python
+
+import sys;
+import os;
+
+from utils import cluster_null as null;
+
+from visualization import circos_data as cdata;
+from visualization import circos_region as cr;
+from visualization import circos_chr    as cc;
+
+import data;
+import proteny as ps;
+import postprocessing as pp;
+
+###############################################################################
+
+def usage(arg0):
+  print "Run the core Protreny algorithm";
+  print "Usage %s <name_org1> <genes_org1> <genome_org1> <name_org2> <genes_org2> <genome_org2> <p-value> <c-thresh> <outdir>";
+  print "";
+  print "  Arguments:";
+  print "    <name_orgX>:   The short name for organism X";
+  print "    <genes_orgX>:  The gene description file for organism X";
+  print "    <genome_orgX>: The genome fasta file for organism X";
+  print "    <p-value>:     The p-value threshold for clusters";
+  print "    <c-thresh>:    The conservation threshold for clusters";
+  print "    <outdir>:      The output directory to use";
+ 
+#edef
+
+###############################################################################
+
+def viz(PR, k, outdir):
+  KS = PR.key_s(k);
+
+  files = cdata.write_data(PR, k, '%s/data' % outdir);
+
+  # Visualize relationships between chromosomes
+  bchrs = ["%s_%s" % (PR.org_names[k['id_b']], str(i)) for i in PR.org_genomes[k['id_b']].Get(0)() ];
+  for i in xrange(PR.org_genomes[k['id_a']].Shape()()):
+    cc.circos_chr(files, bchrs + ["%s_%d" % (PR.org_names[k['id_a']], i+1)], ["%s_%d=0.4r" % (PR.org_names[k['id_a']], i+1)], outdir, "scaffold_%02d_%s" % (i+1, KS) );
+    print "scaffold_%02d_%s" % (i+1, KS);
+  #efor
+
+#edef
+
+###############################################################################
+
+if __name__ == '__main__':
+
+  if len(sys.argv) < 10:
+    usage(sys,argv[0]);
+    sys.exit(1);
+  #fi
+
+  name_org1   = sys.argv[1];
+  genes_org1  = sys.argv[2];
+  genome_org1 = sys.argv[3];
+
+  name_org2   = sys.argv[4];
+  genes_org2  = sys.argv[5];
+  genome_org2 = sys.argv[6];
+  
+  pvalue  = sys.argv[7];
+  cthresh = sys.argv[8];
+  outdir  = sys.argv[9];
+
+  savename = '%s/proteny_data.proteny' % outdir;
+
+    # Load the data
+  org1 = data.read_prepared(name_org1, genes_org1, genome_org1);
+  org2 = data.read_prepared(name_org2, genes_org2, genome_org2);
+
+    # Prepare proteny structure
+  PR = ps.proteny();
+
+    # Add our organisms.
+  id_a = PR.add_org(*org1, isfile=False);
+  id_b = PR.add_org(*org2, isfile=False);
+  
+  ###############################################################################
+  # ANALYSIS
+
+    # Run analysis
+  k = PR.analyze(id_a=id_a, id_b=id_b, cut='deeper_greater', nd=null.cluster_null_score_strict_smart, alpha=pvalue, ngenes_threshold=2, conservation_ratio=cthresh);
+
+    # Save! It's important!
+  PR.save(savename);
+
+    # Produce circos visualizations
+  #viz(PR, k, outdir);
+
+#fi
---
+++ run_proteny.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+###############################################################################
+
+function usage(){
+
+  arg0="$0";
+
+  echo "Run the proteny software";
+  echo "  Usage: $arg0 <name_org1> <genes_org1> <genome_org1> <name_org2> <genes_org2> <genome_org2> <p-value> <c-thresh> <outdir>";
+  echo "";
+  echo "  Arguments:";
+  echo "    <name_orgX>:   The short name for organism X";
+  echo "    <genes_orgX>:  The gene description file for organism X";
+  echo "    <genome_orgX>: The genome fasta file for organism X";
+  echo "    <p-value>:     The p-value threshold for clusters";
+  echo "    <c-thresh>:    The conservation threshold for clusters";
+  echo "    <outdir>:      The output directory to use";
+  echo "    <ncores>:      The number of cores available to the ipcluster";
+
+}
+
+###############################################################################
+
+if [ "$#" -ne 10 ]; then
+  echo $@;
+  usage $0;
+  exit 1;
+fi
+
+###############################################################################
+
+name_org1="$1"
+name_org2="$4"
+
+genes_org1="$2";
+genes_org2="$5";
+
+genome_org1="$3";
+genome_org2="$6";
+
+pvalue="$7";
+cthresh="$8";
+outdir="$9";
+ncores="${10}";
+
+###############################################################################
+echo "STARTING IPcluster";
+ipcluster start -n "$ncores";
+
+###############################################################################
+echo "RUNNING PROTENY";
+mkdir -p "$outdir/data"; 
+./run_proteny.py "$name_org1" "$genes_org1" "$genome_org1" "$name_org2" "$genes_org2" "$genome_org2" "$pvalue" "$cthresh" "$outdir";
+
+###############################################################################
+echo "RUNNING CIRCOS"
+cp "./circos_run" "$outdir";
+cd "$outdir" & ./circos_run;
+
+###############################################################################
+echo "COMPLETE";
